731f9fe6d7227d2d796f77fa9dd6b021112aa47f
Kaavya Antony <ksenthil@conncoll.edu>
2018-10-10 16:36:21 -0400

    phase 1

--- phase1.py
+++ phase1.py
@@ -8,6 +8,7 @@ import queue # storing the processes
 import csv # reading into data
 import pandas as pd # data processing
 
+# create a Process class with parameters priority (int), ID (string or int), owner (string), and memory (int)
 class Process(object):
     def __init__(self, priority=100, ID=0, owner="", memory=0):
     	self.priority = priority
@@ -19,13 +20,16 @@ class Process(object):
         return cmp(self.priority, other.priority)
   
 def main():
+	# reads in CSV file with the processes
 	df = pd.read_csv("/Users/Kaavya/Documents/OS/OS2018/processes.csv")
 
+	# gets columns of each processes' parameters
 	ids = df["ID"]
 	priorities = df["priority"]
 	owners = df["owner"]
 	memory = df["memory_required"]
 
+	# checks to see if the IDs are valid (either int or string)
 	valid_ids = []
 	for i in range(len(ids)):
 		try:
@@ -34,6 +38,7 @@ def main():
 			valid_ids.append(str(ids[i]))
 	#print(valid_ids)
 	 
+	# checks to see if the priorities are valid (either a valid int or we make the priority 100 to give the process a low priority)
 	valid_priority = []
 	for i in range(len(priorities)):
 		try:
@@ -43,6 +48,7 @@ def main():
 			valid_priority.append(100)
 	#print(valid_priority)
 
+	# checks to see if the owners are valid 
 	valid_owners = []
 	for i in range(len(owners)):
 		try:
@@ -51,6 +57,7 @@ def main():
 			valid_owners.append(str(owners[i]))
 	#print(valid_owners)
 
+	# checks to see if the memory is valid, if not we make the memory 0
 	valid_memory = []
 	for i in range(len(memory)):
 		try:
@@ -60,12 +67,15 @@ def main():
 			valid_memory.append(0)
 	#print(valid_memory)
 
+	# creates a priority queue L that will store the processes
 	L = queue.PriorityQueue()
 
+	# if the processes are valid, add them to the queue
 	for i in range(len(valid_ids)):
 		#process1 = Process(valid_priority[i], valid_ids[i], valid_owners[i], valid_memory[i])
 		L.put((valid_priority[i], valid_ids[i], valid_owners[i], valid_memory[i])) 
 
+	# print all the processes
 	while not L.empty():
 		process = L.get()
 		print ('Process:', process)
